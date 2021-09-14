3946b681c3cda23396754a6330ad05728539ea41
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-15 08:58:07 +0900

    [Fix] - Fixed that the application did not launch

--- app/src/main/java/com/example/navigationcomponent_fragment_example/FirstFragment.kt
+++ app/src/main/java/com/example/navigationcomponent_fragment_example/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.example.navigationcomponent_fragment_example
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,7 +39,11 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_first, container, false)
-        view.findViewById<TextView>(R.id.navigateToFirst).setOnClickListener{Navigation.findNavController(view).navigate(R.id.navigateToSecond)}
+        view.setOnClickListener{
+            Log.d("FirstFragment", "FragmentFirst-MoveButton Clicked")
+            findNavController().navigate(R.id.navigateToSecond)
+        }
+//        view.findViewById<TextView>(R.id.navigateToFirst).setOnClickListener{Navigation.findNavController(view).navigate(R.id.navigateToSecond)}
 //        view.textView1.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigateToSecond)}
 
         return view
--- app/src/main/java/com/example/navigationcomponent_fragment_example/SecondFragment.kt
+++ app/src/main/java/com/example/navigationcomponent_fragment_example/SecondFragment.kt
@@ -1,12 +1,14 @@
 package com.example.navigationcomponent_fragment_example
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,7 +39,11 @@ class SecondFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_second, container, false)
-        view.findViewById<TextView>(R.id.navigateToSecond).setOnClickListener{Navigation.findNavController(view).navigate(R.id.navigateToFirst)}
+        view.setOnClickListener{
+            Log.d("SecondFragment", "FragmentSecond-MoveButton Clicked")
+            findNavController().navigate(R.id.navigateToFirst)
+        }
+//        view.findViewById<TextView>(R.id.navigateToSecond).setOnClickListener{Navigation.findNavController(view).navigate(R.id.navigateToFirst)}
         return view
     }
 
